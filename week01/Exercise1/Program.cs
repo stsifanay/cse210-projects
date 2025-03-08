96839c7517f69408db3395e26a204561d6eeebb1
StefanTsifanay <stefantsifanay@gmail.com>
2025-03-08 02:49:24 -0500

    Finish prep 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,5 +6,15 @@ class Program
     {
         //This is prep 2
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("This is C#.");
+
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.Write($"Your name is {firstname}, {firstname} {lastname}");
+
     }
 }
