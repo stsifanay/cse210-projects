3529666601fb6548d99e83340c59dd205751ea21
StefanTsifanay <stefantsifanay@gmail.com>
2025-03-09 01:44:03 -0500

    Finished prep 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+     
+     //Creating the list syntax
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+
+        //Asking user to enter a number with an option to quit when 0
+        Console.Write("Please enter a number, 0 to quit ");
+        string userResponse = Console.ReadLine();
+        //converting the answer from string to integer 
+        userNumber = int.Parse(userResponse);
+
+        if (userNumber != 0)
+            {
+            numbers.Add(userNumber);
+            }
+            
+        //Compute the sum of words
+        int wordSum = 0;
+        foreach (int number in numbers)
+            {
+            wordSum += number;
+            }
+
+        Console.WriteLine($"The sum is: {wordSum}");
+
+        //Compute the average 
+        float average = ((float)wordSum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //Compute the max
+        int max = numbers[0];
+        foreach (int number in numbers)
+        if (number > max)
+            {
+            max = number;
+            }
+        Console.WriteLine($"The max is: {max}");
+        }
     }
 }
